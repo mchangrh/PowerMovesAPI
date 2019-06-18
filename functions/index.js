6a76bec422dcb870ab39ec39e2b2468095f69845
Michael <mchangrh@gmail.com>
2019-06-18 03:40:28 -0400

    added jsonp case

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,15 @@ exports.powerMove = functions.https.onRequest((request, response) => {
   response.status(200).send(powerMove)
 })
 
+exports.powerMoveP = functions.https.onRequest((request, response) => {
+  const powerMove = getRandom(powerMoveList)
+  // craft response for JSONP
+  start = 'powerMoves=('
+  end = ');'
+  finalResponse = start + JSON.stringify(powerMove) + end
+  response.status(200).send(finalResponse)
+})
+
 function getRandom(array) {
   let index = Math.floor(Math.random() * array.length);
   return array[index];
